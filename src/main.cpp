291b54bb4877e4a15b0167b30ce46e31a07a8707
Patrick Hoffmann <pat@thats-software.com>
2023-06-07 03:42:40 +0200

    minor review

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,14 @@
-/**
- * Author Teemu Mäntykallio
- * Initializes the library and runs the stepper
- * motor in alternating directions.
- */
-
 #include <TMCStepper.h>
 #include <AccelStepper.h>
 
-#define STEP_PIN 14
-#define DIR_PIN 12
-#define CS_PIN 5   // Chip select
-#define SW_MOSI 23 // Software Master Out Slave In (MOSI)
-#define SW_MISO 19 // Software Master In Slave Out (MISO)
-#define SW_SCK 18  // Software Slave Clock (SCK)
+#define STEP_PIN 14 // Step slope
+#define DIR_PIN 12  // Direction
+#define CS_PIN 5    // Chip select
+#define SW_MOSI 23  // Software Master Out Slave In (MOSI)
+#define SW_MISO 19  // Software Master In Slave Out (MISO)
+#define SW_SCK 18   // Software Slave Clock (SCK)
 
 #define R_SENSE 0.11f // Match to your driver
-                      // SilentStepStick series use 0.11
-                      // UltiMachine Einsy and Archim2 boards use 0.2
-                      // Panucatt BSD2660 uses 0.1
-                      // Watterott TMC5160 uses 0.075
 #define STALL_VALUE 0 // [-64..63]
 
 // Select your stepper driver type
@@ -75,8 +65,8 @@ void loop()
     dir = -1;
     stepper.disableOutputs();
     delay(1000);
-    stepper.moveTo(0 * steps_per_mm);
     stepper.setMaxSpeed(100 * steps_per_mm);
+    stepper.moveTo(0 * steps_per_mm);
     stepper.enableOutputs();
     n = max_n;
   }
