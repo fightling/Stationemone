b056acfc0ce2966189067baa3aff6677b72045a1
Patrick Hoffmann <pat@thats-software.com>
2023-06-09 18:29:29 +0200

    random

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,12 @@ void setup()
   // SPI.begin();    // SPI drivers
   driver.begin();             //  SPI: Init CS pins and possible SW SPI pins
                               //  driver.push();
-  driver.rms_current(1900);   // Set motor RMS current
+  driver.rms_current(900);    // Set motor RMS current
   driver.en_pwm_mode(true);   // Toggle stealthChop on TMC2130/2160/5130/5160
   driver.pwm_autoscale(true); // Needed for stealthChop
   driver.microsteps(16);      // Set microsteps to 1/16th
 
-  stepper.setMaxSpeed(100 * steps_per_mm);
+  stepper.setMaxSpeed(500 * steps_per_mm);
   stepper.setAcceleration(50 * steps_per_mm);
   // stepper.setEnablePin(EN_PIN);
   stepper.setPinsInverted(false, false, true);
@@ -48,14 +48,20 @@ int dir = 0;
 int max_n = 10000;
 int n = 0;
 
+uint32_t pause_in_min = 5000;
+uint32_t pause_in_max = 10000;
+
+uint32_t pause_out_min = 0;
+uint32_t pause_out_max = 30000;
+
 void loop()
 {
   if (stepper.currentPosition() <= 0 && dir != 1)
   {
     dir = 1;
     stepper.disableOutputs();
-    delay(1000);
-    stepper.setMaxSpeed(10 * steps_per_mm);
+    delay(random(pause_out_min, pause_out_max));
+    stepper.setMaxSpeed(500 * steps_per_mm);
     stepper.moveTo(distance * steps_per_mm);
     stepper.enableOutputs();
     n = max_n;
@@ -64,16 +70,17 @@ void loop()
   {
     dir = -1;
     stepper.disableOutputs();
-    delay(1000);
-    stepper.setMaxSpeed(100 * steps_per_mm);
+    delay(random(pause_in_min, pause_in_max));
+    stepper.setMaxSpeed(2 * steps_per_mm);
     stepper.moveTo(0 * steps_per_mm);
     stepper.enableOutputs();
     n = max_n;
   }
-  if (++n > max_n)
-  {
-    n = 0;
-    Serial.println(stepper.currentPosition() / steps_per_mm);
-  }
+  /*  if (++n > max_n)
+    {
+      n = 0;
+      Serial.println(stepper.currentPosition() / steps_per_mm);
+    }
+    */
   stepper.run();
 }
