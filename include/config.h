9ef3d18bcfb96a9909a3b7e4f87cd98e1ba1b94e
Patrick Hoffmann <pat@thats-software.com>
2023-06-11 18:04:14 +0200

    faster in move

--- include/config.h
+++ include/config.h
@@ -1,13 +1,13 @@
 #include "Stepper.h"
 
 Stepper::Parameters params = {
-    .max_speed = 500.0,
-    .acceleration = 50.0,
-    .rms_current = 900,
+    .max_speed = 1000.0,
+    .acceleration = 200.0,
+    .rms_current = 800,
     .distance_mm = 21,
     .steps_per_mm = 215,
     .pause_in = {5000, 10000}, // (min/max)
     .pause_out = {0, 30000},   //  (min/max)
-    .speed_in = 500,
+    .speed_in = 800,
     .speed_out = 2,
 };
